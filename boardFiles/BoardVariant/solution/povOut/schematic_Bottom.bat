484c2ef20e312921b0b621dd6fcb9768c899a6a4
pixma38@gmail.com <pixma38@gmail.ccom>
2014-09-14 19:19:50 +0530

    normal Push.

---
+++ boardFiles/BoardVariant/solution/povOut/schematic_Bottom.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"C:\Program Files\POV-Ray\v3.7\bin\pvengine.exe" +W1600 +H1200 +A0.3 +FN +L"c:/windows/fonts" +L"C:/Program Files (x86)/eagle3d/ulp//../povray" +O"D:/myTrace/boardFiles/BoardVariant/solution/povOut/schematic_bottom.png" /RENDER "D:/myTrace/boardFiles/BoardVariant/solution/povOut/schematic_bottom.pov" 
